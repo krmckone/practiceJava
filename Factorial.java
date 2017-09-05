78733881bc7defefb495fe181d527f01fcca4008
kwajandsatan <kmckone1825@gmail.com>
2017-09-05 10:28:12 -0500

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+//Factorial.java
+public class Factorial{
+	
+	public static int factorial(int i){
+		if (i == 1){
+			return i;
+		} else{
+			return i * factorial(i - 1);
+		}
+	}
+
+	public static void main(String[] args){
+		int input = Integer.parseInt(args[0]);
+		System.out.println(factorial(input));
+	}
+}
+
